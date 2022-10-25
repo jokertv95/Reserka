26d8b2ba93b147fc288398fd76acf3ccaf2a9a8c
AlgisSimkaitis <algis@euroexchange.lv>
2022-10-25 11:52:25 +0300

    FIX: Font isue

--- js/script.js
+++ js/script.js
@@ -7,33 +7,7 @@ btn.addEventListener("click", () => {
   nav.classList.toggle("hidden");
 });
 
-// FAQ Accordeon
-const accordionItemHeaders = document.querySelectorAll(
-  ".accordion-item-header"
-);
-
-accordionItemHeaders.forEach((accordionItemHeader) => {
-  accordionItemHeader.addEventListener("click", (event) => {
-    const currentlyActiveAccordionItemHeader = document.querySelector(
-      ".accordion-item-header.active"
-    );
-    if (
-      currentlyActiveAccordionItemHeader &&
-      currentlyActiveAccordionItemHeader !== accordionItemHeader
-    ) {
-      currentlyActiveAccordionItemHeader.classList.toggle("active");
-      currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
-    }
-
-    accordionItemHeader.classList.toggle("active");
-    const accordionItemBody = accordionItemHeader.nextElementSibling;
-    if (accordionItemHeader.classList.contains("active")) {
-      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
-    } else {
-      accordionItemBody.style.maxHeight = 0;
-    }
-  });
-});
+//
 
 //
 
