7c7ee95290619e792771602697c12d7bea14788c
AlgisSimkaitis <algis@euroexchange.lv>
2022-10-28 13:35:12 +0300

    FIX:CTA

--- js/cta-slider.js
+++ js/cta-slider.js
@@ -1,5 +1,22 @@
 const slider = tns({
   container: ".cta_carousel",
-  slideBy: "1",
+  slideBy: "page",
+  controls: false,
+  touch: true,
   items: "1",
+  autoplay: true,
+  responsive: {
+    425: {
+      items: 1,
+      fixedWidth: 300,
+    },
+    768: {
+      items: 1,
+      fixedWidth: 534,
+    },
+    1000: {
+      items: 1,
+      fixedWidth: 534,
+    },
+  },
 });
